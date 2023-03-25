3a5eb56dd7e40352244e6803a9d86810299ff82c
Brent Buchholz <brentbuchholz@gmail.com>
2023-03-25 10:56:08 -0500

    Moved links into about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <AboutLinks />
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AboutLinks from "@/components/AboutLinks.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "AboutView",
+  components: {
+    AboutLinks,
+  },
+});
+</script>
