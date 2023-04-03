263d205f77e70571abf78e623e294ba13012d090
Brent Buchholz <brentbuchholz@gmail.com>
2023-04-02 21:43:19 -0500

    Add Myriad Nav Component

--- src/views/MyriadView.vue
+++ src/views/MyriadView.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="about">
-    <AboutLinks />
-    <img
+  <div class="myriad">
+    <MyriadLinks />
+    <!-- <img
       alt="A Technician In A Cyberpunk Occult Style Working On An Engine"
       src="../assets/Imagine_Occult-Technician_with-stamp.jpg"
-    />
+    /> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import AboutLinks from "@/components/AboutLinks.vue"; // @ is an alias to /src
+import MyriadLinks from "@/components/MyriadLinks.vue"; // @ is an alias to /src
 
 export default defineComponent({
-  name: "AboutView",
+  name: "MyriadView",
   components: {
-    AboutLinks,
+    MyriadLinks,
   },
 });
 </script>
